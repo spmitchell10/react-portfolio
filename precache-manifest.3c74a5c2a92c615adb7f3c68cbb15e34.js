8733bdf5d63d6411af32645f3d19c1328c54d9cd
Stephen Mitchell <stephenmitchell@Stephens-MBP.home>
2021-02-20 17:11:42 -0600

    Updates

---
+++ precache-manifest.3c74a5c2a92c615adb7f3c68cbb15e34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b7d61006745ad4a84c7d0db0e731ae",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ca5fcea6a1d10d499dc0",
+    "url": "/react-portfolio/static/css/main.1b168bff.chunk.css"
+  },
+  {
+    "revision": "9ead576a0db8e87f93fe",
+    "url": "/react-portfolio/static/js/2.70cdbb2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.70cdbb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5fcea6a1d10d499dc0",
+    "url": "/react-portfolio/static/js/main.e9058dbb.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "1b01c10ac4958930d0694f7d0221ceb4",
+    "url": "/react-portfolio/static/media/aggiebound.1b01c10a.png"
+  },
+  {
+    "revision": "7f6e0e36ad9d55c1d6c811d5c40eee31",
+    "url": "/react-portfolio/static/media/bluelava.7f6e0e36.png"
+  },
+  {
+    "revision": "3b75e2812ade9bb5a71b0fb3e53b6f14",
+    "url": "/react-portfolio/static/media/main_IMG.3b75e281.jpeg"
+  },
+  {
+    "revision": "d0e4a70bc939ceb5d16d4f32a01f4227",
+    "url": "/react-portfolio/static/media/openLP.d0e4a70b.png"
+  },
+  {
+    "revision": "98ed68feabf373a77ff0d08ee2cafdf3",
+    "url": "/react-portfolio/static/media/stephen_mitchell_resume.98ed68fe.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.8134177adf19ae0fef69399013e581ca.js"
+  "/react-portfolio/precache-manifest.3c74a5c2a92c615adb7f3c68cbb15e34.js"
 );
 
 self.addEventListener('message', (event) => {
